79eb52f329b7a915a9b84fce9cf1f838ec0c4cf7
Anwar Rahim <anwargithub@gmail.com>
2022-12-13 15:01:55 +0500

    add the first and last name and also mention the comments

--- python3_projects/Block_Letters/README.md
+++ python3_projects/Block_Letters/README.md
@@ -1,5 +1,18 @@
+# consider this image
+![img_3.png](img_3.png)
+# reademe
 ASCII art is a graphic design technique that uses computers for presentation and consists of pictures pieced together from individual characters.
 
 Write a Python program called initials.py that displays the initials of your name in block letters as shown and dip your toes into ASCII art.
 ![img.png](img.png)
 Happy Coding!
+
+# 1) first task to check
+  ![img_1.png](img_1.png)
+# 2) second task to check
+  ![img_2.png](img_2.png)
+
+
+
+ 
+
--- python3_projects/Block_Letters/initials.py
+++ python3_projects/Block_Letters/initials.py
@@ -1,3 +1,38 @@
-#Note.........
+# Note.........
 # read the file README.md
 
+print(" sss " + " L ")
+print("s   s" + " L ")
+print("s    " + " L ")
+print(" sss " + " L ")
+print("    s" + " L ")
+print("s   s" + " L ")
+print(" sss " + " LLLLL ")
+
+#for line space we use print
+print()
+print()
+print()
+
+# my first
+
+print("  A  " + "  " + "N   N" + "  " + "W   W" + "  " + "  A  " + "  " + "RRRR ")
+print(" A A " + "  " + "NN  N" + "  " + "W   W" + "  " + " A A " + "  " + "R   R")
+print("A   A" + "  " + "N N N" + "  " + "W   W" + "  " + "A   A" + "  " + "R   R")
+print("AAAAA" + "  " + "N  NN" + "  " + "W W W" + "  " + "AAAAA" + "  " + "RRRR ")
+print("A   A" + "  " + "N   N" + "  " + "WW WW" + "  " + "A   A" + "  " + "R R  ")
+print("A   A" + "  " + "N   N" + "  " + "WW WW" + "  " + "A   A" + "  " + "R  R ")
+print("A   A" + "  " + "N   N" + "  " + "W   W" + "  " + "A   A" + "  " + "R   R")
+
+print()
+print()
+print()
+# Last Name
+print("RRRR " + "  " + "  A  " + "  " + "H   H" + "  " + "IIIII" + "  " + "M   M")
+print("R   R" + "  " + " A A " + "  " + "H   H" + "  " + "  I  " + "  " + "MM MM")
+print("R   R" + "  " + "A   A" + "  " + "H   H" + "  " + "  I  " + "  " + "MM MM")
+print("RRRR " + "  " + "AAAAA" + "  " + "HHHHH" + "  " + "  I  " + "  " + "M M M")
+print("R R  " + "  " + "A   A" + "  " + "H   H" + "  " + "  I  " + "  " + "M   M")
+print("R  R " + "  " + "A   A" + "  " + "H   H" + "  " + "  I  " + "  " + "M   M")
+print("R   R" + "  " + "A   A" + "  " + "H   H" + "  " + "IIIII" + "  " + "M   M")
+# A fun fact about me
