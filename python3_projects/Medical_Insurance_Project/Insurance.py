2fc232ed860f512dc5792aeb104212a8d896a4b5
Anwar Rahim <anwargithub@gmail.com>
2024-03-18 12:01:46 +0500

    add new varibales

--- python3_projects/Medical_Insurance_Project/Insurance.py
+++ python3_projects/Medical_Insurance_Project/Insurance.py
@@ -1,1 +1,8 @@
 age =22
+sex = "Male"
+name = input("Enter Your Name")
+weight = input("Enter your weight in kg")
+height = input("Enter your height in meter")
+bmi = weight/(height)**2
+print(name + "BMI is this :"+ bmi)
+
