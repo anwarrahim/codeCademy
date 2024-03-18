f758fb8a03d69fc2ac33de3530382c82d76c8ba9
Anwar Rahim <anwargithub@gmail.com>
2024-03-18 12:12:03 +0500

    add

--- python3_projects/Medical_Insurance_Project/Insurance.py
+++ python3_projects/Medical_Insurance_Project/Insurance.py
@@ -1,8 +1,10 @@
 age =22
 sex = "Male"
 name = input("Enter Your Name")
-weight = input("Enter your weight in kg")
-height = input("Enter your height in meter")
-bmi = weight/(height)**2
+
+weight = float(input("Enter your weight in kg"))
+height = float(input("Enter your height in meter"))
+bmi = weight/( height ** 2)
 print(name + "BMI is this :"+ bmi)
 
+ 
