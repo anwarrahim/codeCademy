b55d3a8ea69e837700d52b59c3299c0afe228eb0
Anwar Rahim <anwargithub@gmail.com>
2024-03-18 12:18:44 +0500

    add new things

--- python3_projects/Medical_Insurance_Project/Insurance.py
+++ python3_projects/Medical_Insurance_Project/Insurance.py
@@ -1,10 +1,9 @@
 age =22
 sex = "Male"
 name = input("Enter Your Name")
-
+#  if we not add float then this show error because this think height and weight is string and in bmi formula we are trying mathimatical operation.
 weight = float(input("Enter your weight in kg"))
-height = float(input("Enter your height in meter"))
+height = float(input("Enter you r height in meter"))
 bmi = weight/( height ** 2)
-print(name + "BMI is this :"+ bmi)
-
- 
+rounded_bmi= round(bmi ,2)
+print(name + "BMI is this :"+ str(rounded_bmi))
